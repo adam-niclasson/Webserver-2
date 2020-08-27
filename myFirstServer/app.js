4a2354b3c2b733abc61161dc12de0e3ca005f5b8
adam-niclasson <54893668+adam-niclasson@users.noreply.github.com>
2020-08-27 14:43:39 +0200

    yes

---
+++ myFirstServer/app.js
@@ -0,0 +1,21 @@
+let math1 = 18
+
+let math2 = 4
+
+console.log(math1, math2)
+
+math1 + math2
+
+console.log(math1 + math2)
+
+math1 * math2
+
+console.log(math1 * math2)
+
+math1 / math2
+
+console.log(math1 / math2)
+
+math1 - math2
+
+console.log(math1 - math2)
