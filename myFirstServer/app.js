438f3ee7ae6269babf425994f9631870af2aad04
adam-niclasson <adam-niclasson@users.noreply.github.com>
2020-09-09 13:32:41 +0200

    yes

--- myFirstServer/app.js
+++ myFirstServer/app.js
@@ -3,13 +3,17 @@ const labb = require('./labb')
 const app = express()
 const port = 3000
 
-labb.funk1("Niklas QuadKaKa")
+const clientDir = __dirname + '\\client\\'
 
-//let sum = labb.add(1, 2) //3
-//let dif = labb.sub(1, 2) //3
-//let prod = labb.multi(5, 2) //3
+//labb.funk1("Niklas QuadKaKa")
+
+let sum = labb.add(1, 2) //3
+let dif = labb.sub(1, 2) //3
+let prod = labb.multi(5, 2) //3
 
 console.log("Summan är: " + labb.add(1, 2) + " Differansen:" + labb.sub(1, 2) + " Produketen; " + labb.multi(5, 2))
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => res.sendFile(__dirname + '\\client\\index.html'))
+app.get('/mainStyle', (req, res) => res.sendFile(clientDir + 'shit.css'))
+
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
