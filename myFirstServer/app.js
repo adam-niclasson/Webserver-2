6c08ad5d5f2af25273353bcce186575d8d33c1c2
adam-niclasson <adam-niclasson@users.noreply.github.com>
2020-09-30 14:07:59 +0200

    yes

--- myFirstServer/app.js
+++ myFirstServer/app.js
@@ -3,29 +3,13 @@ const labb = require('./labb')
 const app = express()
 const port = 3000
 
-const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/test', { useNewUrlParser: true });
-
-const db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function () {
-  // we're connected!
-});
+const clientDir = __dirname + "\\client\\"
 
 app.use(express.json())
 app.use(express.urlencoded())
 
-
 let no = "YOU SHALL NOT PASS!"
 let yes = "You may pass into mother russia"
-
-
-const clientDir = __dirname + '\\client\\'
-const personSchema = new mongoose.Schema({
-    name: String,
-    email: String,
-    age: Number
-  });
 
 const Person = mongoose.model('Person', personSchema);
 
@@ -37,7 +21,9 @@ let prod = labb.multi(5, 2) //3
 
 console.log("Summan är: " + labb.add(1, 2) + " Differansen:" + labb.sub(1, 2) + " Produketen; " + labb.multi(5, 2))
 
-app.get('/', (req, res) => res.sendFile(clientDir + 'index.html'))
+app.get('/', (req, res) => {
+    res.sendFile(clientDir + 'index.html')
+})
 
 app.get('/mainStyle', (req, res) => {
     res.sendFile(clientDir + 'shit.css')
@@ -63,6 +49,16 @@ app.post('/', function (req, res) {
     else {
         console.log("You don't exist and your life is a lie.")
     }
+
+    console.log(req.body.name)
+    console.log(req.body.email)
+
+    databaseModule.storePerson(req.body.name, req.body.email, req.body.age)
+
+    res.redirect('/')
+
 })
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}!`)
+})
