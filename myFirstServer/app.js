853c2b76aec181e04708090b6f78b7f4c90e3a2f
adam-niclasson <54893668+adam-niclasson@users.noreply.github.com>
2020-10-01 14:03:20 +0200

    yes

--- myFirstServer/app.js
+++ myFirstServer/app.js
@@ -1,41 +1,23 @@
 const express = require('express')
-const labb = require('./labb')
+const databaseModule = require('./module.js')
 const app = express()
 const port = 3000
 
-const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/test', { useNewUrlParser: true });
-
-const db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function () {
-  // we're connected!
-});
+const clientDir = __dirname + "\\client\\"
 
 app.use(express.json())
 app.use(express.urlencoded())
 
-
 let no = "YOU SHALL NOT PASS!"
 let yes = "You may pass into mother russia"
 
-
-const clientDir = __dirname + '\\client\\'
-const personSchema = new mongoose.Schema({
-    name: String,
-    email: String,
-    age: Number
-  });
-
-const Person = mongoose.model('Person', personSchema);
-
 //labb.funk1("Niklas QuadKaKa")
 
-let sum = labb.add(1, 2) //3
-let dif = labb.sub(1, 2) //3
-let prod = labb.multi(5, 2) //3
+// let sum = labb.add(1, 2) //3
+// let dif = labb.sub(1, 2) //3
+// let prod = labb.multi(5, 2) //3
 
-console.log("Summan är: " + labb.add(1, 2) + " Differansen:" + labb.sub(1, 2) + " Produketen; " + labb.multi(5, 2))
+// console.log("Summan är: " + labb.add(1, 2) + " Differansen:" + labb.sub(1, 2) + " Produketen; " + labb.multi(5, 2))
 
 app.get('/', (req, res) => res.sendFile(clientDir + 'index.html'))
 
@@ -63,6 +45,15 @@ app.post('/', function (req, res) {
     else {
         console.log("You don't exist and your life is a lie.")
     }
+
+    console.log(req.body.name)
+    console.log(req.body.email)
+  
+    databaseModule.storePerson(req.body.name, req.body.email, req.body.age)
+  
+    res.redirect('/')
 })
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}!`)
+})
